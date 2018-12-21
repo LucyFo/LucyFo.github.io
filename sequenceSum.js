70521cf404c6e10f41e5d042e03c01c0c74c49f3
Lucy <33134227+LucyFo@users.noreply.github.com>
2018-12-21 12:41:53 +0200

    Update sequenceSum.js

--- sequenceSum.js
+++ sequenceSum.js
@@ -1,6 +1,5 @@
 const sequenceSum = (begin, end) => {
-  // BEGIN (write your solution here)
-  if (begin > end) {
+    if (begin > end) {
     return NaN;
   }
   else if (begin === end) {
@@ -10,7 +9,5 @@ const sequenceSum = (begin, end) => {
     return begin + sequenceSum(begin + 1, end);
   }
 
-  // END
-};
+  };
 
-export default sequenceSum;
